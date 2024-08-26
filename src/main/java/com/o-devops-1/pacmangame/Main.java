fbf41588049688f978e5319592c18f9561210591
O-DevOps-1 <ofattahserir@gmail.com>
2024-08-26 22:45:23 +0200

    Update Main.java

---
+++ src/main/java/com/o-devops-1/pacmangame/Main.java
@@ -0,0 +1,22 @@
+package com.o-devops-1.pacmangame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game.fxml"));
+        Pane root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
